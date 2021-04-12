bab248e94e3ba0cc06bf66967700df1adf1cca18
Vallghall <romangusev2001@mail.ru>
2021-04-12 21:20:39 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Queue.h"
+int main() {
+    Queue<string> list;
+    list.push("Van");
+    list.push("Performance");
+    list.push("Artist");
+    cout << "The lenghth os the queue is " << list.GetSize() << endl;
+    cout << "The top element in queue is " << list.peek() << endl;
+    cout << "The first list is:\t" << list;
+    Queue<string> fist = list;
+    cout << "The copied list is:\t" << fist;
+    for (int i = 0; i <= 2; i++)
+    {
+        cout  << list.pop() << " is out\n";
+    }
+    list.push("Gym");
+    list.push("LeatherStuff");
+    list.push("Chains");
+    cout << list;
+    Queue<string> mist = move(list);
+    cout << "THe first list after removal: "<< list;
+    cout << "The moved list is:\t" << mist;
+}
